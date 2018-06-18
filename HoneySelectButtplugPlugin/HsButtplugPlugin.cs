161e8717a4eb8045989ea7ff7cfc013917621866
blackspherefollower <darktech@black-sphere.co.uk>
2018-06-18 13:59:37 +0100

    Detecting character orgasm
    This triggers the vibration to run at full power for the length of the animation. Sweet spot now only runs the vibe at 50%

--- HoneySelectButtplugPlugin/HsButtplugPlugin.cs
+++ HoneySelectButtplugPlugin/HsButtplugPlugin.cs
@@ -20,6 +20,8 @@ namespace HoneySelectButtplugPlugin
         private ButtplugWSClient _bpClient;
 
         private bool _vibrate;
+        private bool _inOrgasm;
+        private DateTime _endOrgsam;
 
         public void OnApplicationStart()
         {
@@ -63,6 +65,7 @@ namespace HoneySelectButtplugPlugin
             }
 
             bool hit = false;
+            bool orgasm = false;
             string data = "";
             GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
             foreach (GameObject go in allObjects)
@@ -80,30 +83,50 @@ namespace HoneySelectButtplugPlugin
                 foreach (var c in anim.GetCurrentAnimatorClipInfo(0))
                 {
                     data += "\t" + c.clip.name + "\n";
+
+                    // f_heart and m_heart are the sweet spots for the actors
+                    // clip names are either hit or idle
+                    //Found: heart_m - heart_m - 1 looping
+                    //    idle
+                    //Found: heart_f - heart_f - 1 looping
+                    //    hit
                     if ((go.name == "heart_m" || go.name == "heart_f") && c.clip.name == "hit")
                         hit = true;
+
+                    // p_cm_anim and p_cf_anim are the character movement animations
+                    // When orgasming, they are set to M_Orgasm, M_Orgasm, M_Orgasm_out, etc
+                    //Found: p_cm_anim - p_cm_anim - 3.809526
+                    //    M_Orgasm
+                    //    M_Orgasm
+                    //Found: p_cf_anim - p_cf_anim - 3.809526
+                    //    M_Orgasm
+                    //    M_Orgasm
+                    if (!_inOrgasm && (go.name == "p_cf_anim" || go.name == "p_cm_anim") && c.clip.name.Contains("_Orgasm") && !c.clip.name.EndsWith("_A"))
+                    {
+                        orgasm = true;
+                        _endOrgsam = DateTime.Now.AddSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
+                    }
                 }
-
-
-                // f_heart and m_heart are the sweet spots for the actors
-                // clip names are either hit or idle
-                //Found: heart_m - heart_m - 1 looping
-                //idle
-                //Found: heart_f - heart_f - 1 looping
-                //    hit
             }
 
-            if (_vibrate == hit)
+            if (_inOrgasm)
+            {
+                orgasm = _endOrgsam >= DateTime.Now;
+            }
+
+            if (_vibrate == hit && orgasm == _inOrgasm)
             {
                 return;
             }
 
             Console.WriteLine("State changed!\n" + data);
             _vibrate = hit;
+            _inOrgasm = orgasm;
+
             foreach (var dev in _bpClient.Devices)
             {
                 Console.WriteLine("Sending vibrate cmd to " + dev.Name);
-                var res = _bpClient.SendDeviceMessage(dev, new SingleMotorVibrateCmd(dev.Index, _vibrate ? 1 : 0))
+                var res = _bpClient.SendDeviceMessage(dev, new SingleMotorVibrateCmd(dev.Index, _inOrgasm ? 1 : _vibrate ? 0.5 : 0))
                     .Result;
                 if (res.GetType() == typeof(Error))
                 {
